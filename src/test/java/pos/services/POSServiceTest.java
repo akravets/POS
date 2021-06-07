75f6700a40e7f8416a5146d679b5ce7720f74150
Alex Kravets <kravets.dev@gmail.com>
2021-06-06 22:58:35 -0400

    Added POSService.getCommands() test case

--- src/test/java/pos/services/POSServiceTest.java
+++ src/test/java/pos/services/POSServiceTest.java
@@ -8,9 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pos.Application;
+import pos.commands.AbstractCommand;
+import pos.commands.CommandProvider;
 import pos.models.Item;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Set;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
@@ -18,10 +23,21 @@ public class POSServiceTest {
     @Autowired
     public POSService service;
 
-    @SneakyThrows
+    /**
+     * Tests getCommands() method
+     */
     @Test
-    public void getItems(){
+    public void getItems() throws URISyntaxException, IOException {
         List<Item> items = service.getItems();
         Assert.assertEquals(items.size(), 11);
+    }
+
+    /**
+     * Tests getCommands() method
+     */
+    @Test
+    public void getCommands() {
+        Set<AbstractCommand> commands = service.getCommands();
+        Assert.assertEquals(commands.size(), 2);
     }
 }
