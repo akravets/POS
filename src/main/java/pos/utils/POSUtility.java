85e5f18489773739cce9f9774050092f53271e86
noInterestIfPaidInFull <85396913+noInterestIfPaidInFull@users.noreply.github.com>
2021-06-11 06:51:03 -0400

    Update POSUtility.java
    Added comment about search performance

--- src/main/java/pos/utils/POSUtility.java
+++ src/main/java/pos/utils/POSUtility.java
@@ -12,6 +12,8 @@ import java.util.stream.Collectors;
 
 public class POSUtility {
     public static Map<String, List<Item>> groupData(List<Item> items){
+        // TODO: Should really use AVL Tree here to guarantee O(log n) search performance because we map approach it's possible that all
+        // items will be in one bucket (although not likely), and we'll have O(n) then
         return items.stream().collect(Collectors.groupingBy(s -> s.getSku().substring(0, 3)));
         //return items.stream().collect(Collectors.groupingBy(s -> s.getSku().substring(0, 3), Collectors.toMap(s -> s.getSku(), Function.identity())));
     }
